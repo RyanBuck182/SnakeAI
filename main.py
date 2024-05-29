d5acd1d233b1270ab831a06c2b14239b7b949fe5
ryanbuck182@ <ryanbuck182@gmail.com>
2024-05-29 14:11:09 -0400

    Check for main

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from game_handler import GameHandler
 from pygame_game_renderer import PygameGameRenderer
 from pygame_game_controller import PygameGameController
 
-pygame.init()
 
-GRID_DIMENSIONS = Coordinate(32, 32)
-PLAYER_START_POS = Coordinate(0, 0)
-PLAYER_START_LENGTH = 4
-handler = GameHandler(GRID_DIMENSIONS, PLAYER_START_POS, PLAYER_START_LENGTH)
+if __name__ == '__main__':
+    pygame.init()
 
-SCREEN_DIMENSIONS = Coordinate(640, 640)
-renderer = PygameGameRenderer(SCREEN_DIMENSIONS)
+    GRID_DIMENSIONS = Coordinate(32, 32)
+    PLAYER_START_POS = Coordinate(0, 0)
+    PLAYER_START_LENGTH = 4
+    handler = GameHandler(GRID_DIMENSIONS, PLAYER_START_POS, PLAYER_START_LENGTH)
 
-controller = PygameGameController()
+    SCREEN_DIMENSIONS = Coordinate(640, 640)
+    renderer = PygameGameRenderer(SCREEN_DIMENSIONS)
 
-SECONDS_PER_MOVE = 0.15
-game = Game(handler, renderer, controller, SECONDS_PER_MOVE)
-game.run()
+    controller = PygameGameController()
 
-pygame.quit()
+    SECONDS_PER_MOVE = 0.15
+    game = Game(handler, renderer, controller, SECONDS_PER_MOVE)
+    game.run()
+
+    pygame.quit()
