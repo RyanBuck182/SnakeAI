4b3275ba486b2d3b4fcd2e405b4d75532049be4f
ryanbuck182@ <ryanbuck182@gmail.com>
2024-05-29 14:57:33 -0400

    Add framework for NeatGameController

---
+++ neat_game_controller.py
@@ -0,0 +1,15 @@
+import neat
+from input import Input
+from game_controller import GameController
+from game_handler import GameHandler
+
+
+class NeatGameController(GameController):
+    def __init__(self):
+        pass
+
+    def get_input(self) -> Input:
+        pass
+
+    def calculate_input(self, game_handler: GameHandler):
+        pass
