347d071e6c8a54fca8c763bd1ecbe16b9003c3c1
ryanbuck182@ <ryanbuck182@gmail.com>
2024-06-12 20:36:51 -0400

    Add option menu

--- ui.py
+++ ui.py
@@ -9,7 +9,6 @@ from pygame_menu import PygameMenu
 from selectable_menu_element import SelectableMenuElement
 from optioned_menu_element import OptionedMenuElement, MenuElementOption
 
-DEFAULT_SCREEN_DIMENSIONS = Coordinate(640, 640)
 SCREEN_DIMENSION_OPTIONS = [
     MenuElementOption('320x320', Coordinate(320, 320)),
     MenuElementOption('480x480', Coordinate(480, 480)),
@@ -29,9 +28,12 @@ GRID_DIMENSION_OPTIONS = [
     MenuElementOption('128x128', Coordinate(128, 128))
 ]
 
+DEFAULT_SCREEN_DIMENSION_INDEX = 2
+current_screen_dimension_index = DEFAULT_SCREEN_DIMENSION_INDEX
+
 pygame.init()
 pygame.display.set_caption("Snake")
-screen = pygame.display.set_mode(tuple(DEFAULT_SCREEN_DIMENSIONS))
+screen = pygame.display.set_mode(tuple(SCREEN_DIMENSION_OPTIONS[current_screen_dimension_index].value))
 
 
 def QUIT_GAME_FROM_UI():
@@ -43,10 +45,12 @@ def main_menu() -> Callable:
 
     play_snake = SelectableMenuElement("Play Snake", on_select=lambda: pre_game_menu)
     watch_ai = SelectableMenuElement("Watch AI", on_select=lambda: QUIT_GAME_FROM_UI)
+    options = SelectableMenuElement("Options", on_select=lambda: option_menu)
     quit_game = SelectableMenuElement("Quit", on_select=lambda: QUIT_GAME_FROM_UI)
 
     menu.add_element(play_snake)
     menu.add_element(watch_ai)
+    menu.add_element(options)
     menu.add_element(quit_game)
 
     return menu.load()
@@ -55,13 +59,10 @@ def main_menu() -> Callable:
 def pre_game_menu() -> Callable:
     menu = PygameMenu(screen)
 
-    screen_size = OptionedMenuElement("Screen Size", SCREEN_DIMENSION_OPTIONS, on_select=menu.selection_down)
     grid_size = OptionedMenuElement("Grid Size", GRID_DIMENSION_OPTIONS, on_select=menu.selection_down)
-    play = SelectableMenuElement("Play", on_select=lambda: lambda: play_manual(screen_size.get_selected_value(),
-                                                                               grid_size.get_selected_value()))
+    play = SelectableMenuElement("Play", on_select=lambda: lambda: play_manual(grid_size.get_selected_value()))
     back = SelectableMenuElement("Back", on_select=lambda: main_menu)
 
-    menu.add_element(screen_size)
     menu.add_element(grid_size)
     menu.add_element(play)
     menu.add_element(back)
@@ -69,13 +70,12 @@ def pre_game_menu() -> Callable:
     return menu.load()
 
 
-def play_manual(screen_dimensions: Coordinate, grid_dimensions: Coordinate) -> Callable:
+def play_manual(grid_dimensions: Coordinate) -> Callable:
     player_start_pos = Coordinate(0, 0)
     player_start_length = 4
     handler = GameHandler(grid_dimensions, player_start_pos, player_start_length)
 
     global screen
-    screen = pygame.display.set_mode(tuple(screen_dimensions))
     renderer = PygameGameRenderer(screen)
 
     controller = PygameGameController()
@@ -84,5 +84,29 @@ def play_manual(screen_dimensions: Coordinate, grid_dimensions: Coordinate) -> C
     score = game.run()
     print(score)
 
-    screen = pygame.display.set_mode(tuple(DEFAULT_SCREEN_DIMENSIONS))
     return main_menu
+
+
+def option_menu() -> Callable:
+    global current_screen_dimension_index
+
+    menu = PygameMenu(screen)
+
+    screen_size = OptionedMenuElement("Screen Size", SCREEN_DIMENSION_OPTIONS, on_select=menu.selection_down,
+                                      selected_option=current_screen_dimension_index)
+    apply = SelectableMenuElement("Apply", on_select=lambda: set_screen_size(screen_size.selected_option, option_menu))
+    back = SelectableMenuElement("Back", on_select=lambda: main_menu)
+
+    menu.add_element(screen_size)
+    menu.add_element(apply)
+    menu.add_element(back)
+
+    return menu.load()
+
+
+def set_screen_size(screen_dimension_index: int, menu: Callable):
+    global screen, current_screen_dimension_index
+    screen = pygame.display.set_mode(tuple(SCREEN_DIMENSION_OPTIONS[screen_dimension_index].value))
+    current_screen_dimension_index = screen_dimension_index
+    return menu
+
